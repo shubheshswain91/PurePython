829ea150dade21869f8370bd3b8130c97dab7511
s.swain <shubhesh.swain@intive.com>
2022-11-18 16:14:37 +0100

    Class example7

---
+++ class/c7.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print(isinstance(School_bus, Vehicle))
