49e4dc5c6598580d38c4568d56bd611c3aae284b
s.swain <shubhesh.swain@intive.com>
2022-11-18 16:13:05 +0100

    Class example6

---
+++ class/c6.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount*0.1
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
+
+
