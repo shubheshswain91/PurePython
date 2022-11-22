345f3821406b4515ef4f1d126ad1d1dfb231bfe0
s.swain <shubhesh.swain@intive.com>
2022-11-22 13:51:22 +0100

    First lambda exercise

---
+++ lambda/lmb-1.py
@@ -0,0 +1,18 @@
+
+
+""" 
+Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument, also create a lambda function that multiplies 
+argument x with argument y and print the result.
+ """
+
+
+
+res = lambda x: x+15
+
+print(res(10))
+
+res1 = lambda x,y: x*y
+
+print(res1(2,3))
+
+
