75e9b500e3209ec80de36c8d0e09e12989df1b42
shubheshswain91 <shubhesh.swain@intive.com>
2024-04-20 11:18:03 +0200

    Updated the file

--- dictionary/d1.py
+++ dictionary/d1.py
@@ -8,3 +8,13 @@ print(d)
 
 del d['Raj']
 print(d)
+
+# to check whether a given key already exists in a dictionary.
+def is_key_present(d, k):
+    if k in d:
+        print("present")
+    else:
+        print("Not present") 
+
+is_key_present(d, "Sam")
+is_key_present(d, "Bob")
