fff991e7eceb0c8b45e9ed063ea28817fcf20830
shubheshswain91 <shubheshswain91@gmail.com>
2024-04-22 16:12:11 +0200

    Multiple inheritance

---
+++ inheritance/i4-multiple.py
@@ -0,0 +1,25 @@
+# Multiple inheritance
+
+class Base1(object):
+    def __init__(self):
+        self.str1 = "Geek1"
+        print("Base1")
+
+class Base2(object):
+    def __init__(self):
+        self.str2 = "Geek2"
+        print("Base 2")        
+
+
+class Derived(Base1, Base2):
+    def __init__(self):
+        Base1.__init__(self)
+        Base2.__init__(self)
+        print("Derived")
+
+    def printStrs(self):
+        print(self.str1, self.str2)
+
+
+ob = Derived()
+ob.printStrs()            
