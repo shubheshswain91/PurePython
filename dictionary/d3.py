c37104154628ae1a0a30314298b3d52943e6dade
shubheshswain91 <shubhesh.swain@intive.com>
2024-04-22 14:28:25 +0200

    Added d3

---
+++ dictionary/d3.py
@@ -0,0 +1,20 @@
+# Loop Through a Dictionary
+
+thisdict = dict(name= "John", age = 36, country = "Sweden")
+print(thisdict)
+
+for x in thisdict:
+    print(x)
+
+for x in thisdict:
+    print(thisdict[x])   
+
+
+for x in thisdict.values():
+    print(x)
+
+for x in thisdict.keys():
+    print(x)
+
+for x,y in thisdict.items():
+    print(x,y)    
