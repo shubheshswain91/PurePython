c8a86bfdd0307bc4d833826459e6252a77618d4b
shubheshswain91 <shubheshswain91@gmail.com>
2024-04-22 15:30:27 +0200

    Added child class

--- inheritance/i1.py
+++ inheritance/i1.py
@@ -7,4 +7,44 @@ class Person:
         print(self.firstname, self.lastname)
 
 p1 = Person("Ava", "Mills") 
-p1.printname()           
+p1.printname() 
+
+''' Child class
+To create a class that inherits the functionality from another class, 
+send the parent class as a parameter when creating the child class
+
+'''
+
+class Student(Person):
+#pass
+# When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
+# Note: The child's __init__() function overrides the inheritance of the parent's __init__() function.
+# To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+
+
+
+    def __init__(self, fname, lname):
+        Person.__init__(self, fname, lname)
+            
+
+
+
+'''
+Use the super() Function
+Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
+
+'''
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+x = Student("Mike", "Ola", 2019)
+
+x.welcome()
+
