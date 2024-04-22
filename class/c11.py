630fa7df3a398a9e23cf744fc24352192e089c5e
shubheshswain91 <shubheshswain91@gmail.com>
2024-04-22 15:00:49 +0200

    updated c11

--- class/c11.py
+++ class/c11.py
@@ -11,3 +11,22 @@ class Person:
 p1 = Person("John", 34)
 
 print(p1)
+
+# The self parameter
+
+class Derson:
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
+
+    def myfunc(abc):
+        print(f"Hello my name is {abc.name} and my age is {abc.age} ")
+
+p2 = Derson("Vrek", 22)
+p2.myfunc()  
+
+p2.age = 23
+p2.myfunc()
+
+del p2.age
+del p2
