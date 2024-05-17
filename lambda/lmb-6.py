f822a88c6d21cdb684a8d1657191a9049813b592
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-17 12:44:05 +0200

    Lambda to check startswith string

---
+++ lambda/lmb-6.py
@@ -0,0 +1,9 @@
+# Write a Python program to find whether a given string starts with a given character using Lambda.
+
+
+checkStr = lambda x: True if x.startswith('P') else False
+
+print(checkStr('Panama'))
+
+print(checkStr('Austria'))
+
