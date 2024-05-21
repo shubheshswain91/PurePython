e345c92c91cec9a36cc0ee9ad011ed59b7aac648
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 12:26:14 +0200

    Union Operation

---
+++ Datastructure/sets/s3.py
@@ -0,0 +1,11 @@
+#  Union
+# Two sets can be merged into one set using the union() function or | operator.
+
+people = {"Noddy", "Shin", "Dora"}
+students = {"sam", "sama"}
+  
+# Union using union()
+# function
+population = people.union(students)
+  
+print(population)
