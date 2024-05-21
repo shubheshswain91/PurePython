722b15420f98a9801c0841b092045705fc9b685c
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 09:10:40 +0200

    Basic Tuple

---
+++ Datastructure/Tuple/t1.py
@@ -0,0 +1,10 @@
+ # Write a Python program to create a tuple.
+
+tup = ('English', 'Finish', 'Deutsch', 'Hindi')
+
+print(type(tup))
+print(tup)
+print(tup[0])
+
+# tup[4] = "Arabic" # not possible
+
