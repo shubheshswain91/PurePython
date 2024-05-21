c5914fdc4489954cbd16ed27b097e4e079cf9bb6
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 09:17:45 +0200

    Tuples with various data types

---
+++ Datastructure/Tuple/t2.py
@@ -0,0 +1,16 @@
+# Write a Python program to create a tuple with different data types.
+
+tup = (100, "Euro", [1, "Spain"], {"mykey": "SECRET", "yourKey": "ALEX"}, (101, "Radio"))
+
+
+print(tup)
+
+print(tup[1])
+print(tup[2])
+print(tup[2][1])
+print(tup[3])
+print(tup[3].items())
+print(tup[3].keys())
+print(tup[3].values())
+print(tup[4])
+print(tup[4][0])
