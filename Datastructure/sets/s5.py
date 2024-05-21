363c57d3d4180d4d6a6f4aafec3ee470ee1a3b50
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 12:29:00 +0200

    Difference operation

---
+++ Datastructure/sets/s5.py
@@ -0,0 +1,12 @@
+# Difference
+# To find differences in the elements in both sets. The elements that don’t exist in one set will be returned if we consider them with respect to the second set.
+
+
+set1 = {2,4,6,8,10}
+set2 = {4,6,8,12,14}
+  
+# Difference of two sets
+# using difference() function
+new_set = set1.difference(set2)
+  
+print(new_set)
