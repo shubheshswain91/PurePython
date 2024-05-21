5da96b9049898978db8daf935219f7cf1ab643d5
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 12:27:33 +0200

    Inetersection Operation

---
+++ Datastructure/sets/s4.py
@@ -0,0 +1,8 @@
+# Intersection
+# This can be achieved through the intersection() or & operator, which are used to find out the common elements amongst two sets.
+
+set1 = {"shin", "dora", "cat"}
+set2 = {"shin", "cat", "dog", "lion"}
+  
+new_set = set1.intersection(set2)   
+print(new_set)
