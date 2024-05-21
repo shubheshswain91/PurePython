692f798abfcfd26a92770b52e8e2175c4316edc0
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 12:25:00 +0200

    Adding item to a set

---
+++ Datastructure/sets/s2.py
@@ -0,0 +1,12 @@
+# Creating a Set
+people = {"Oswald", "Doraemon", "Noddy"}
+  
+print("People:", end = " ")
+print(people)
+  
+# This will add Shinchan
+# in the set
+people.add("Shinchan")
+print("\nSet after adding element:", end = " ")
+print(people)#
+
