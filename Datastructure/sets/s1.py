f7f75592864d380c0c9e14b61c6bedd75498fac2
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-21 09:38:18 +0200

    Basic Set

---
+++ Datastructure/sets/s1.py
@@ -0,0 +1,16 @@
+# Create a new empty set
+
+s1 = set()
+print(type(s1))
+print(s1)
+
+# Create a non-empty set 'n' with the elements 0, 1, 2, 3, and 4 using a set literal:
+
+n = set({0,1,2,3,4,5,4})
+print(n)
+a = {0,1,"aaa", "bbb", "aa", "bbb", 0.1, 1}
+print(a)
+
+
+tup = (0,1,2,1,13,18,9)
+print(tup)
